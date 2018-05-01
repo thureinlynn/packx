cf7e24a1d6ce03c777105d9c5b73aa6072f4fd97
thureinlynn <thureinlynn.mm@gmail.com>
2018-05-01 00:20:24 -0400

    2.0

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
+$('.message a').click(function(){
+   $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
+});
+
